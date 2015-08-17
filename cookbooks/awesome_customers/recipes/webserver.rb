9c58c029914db16a0c71384b96ea03a3654a71ef
mosca850 <mosca850@gmail.com>
2015-08-17 18:21:24 +0100

    adding existing cookbooks

---
+++ cookbooks/awesome_customers/recipes/webserver.rb
@@ -0,0 +1,53 @@
+#
+# Cookbook Name:: awesome_customers
+# Recipe:: webserver
+#
+# Copyright (c) 2015 The Authors, All Rights Reserved.
+# Install Apache and start the service.
+httpd_service 'customers' do
+  mpm 'prefork'
+  action [:create, :start]
+end
+
+# Add the site configuration.
+httpd_config 'customers' do
+  instance 'customers'
+  source 'customers.conf.erb'
+  notifies :restart, 'httpd_service[customers]'
+end
+
+# Create the document root directory.
+directory node['awesome_customers']['document_root'] do
+  recursive true
+end
+
+# Write the home page.
+file "#{node['awesome_customers']['document_root']}/index.php" do
+  content '<html>This is a placeholder</html>'
+  mode '0644'
+  owner node['awesome_customers']['user']
+  group node['awesome_customers']['group']
+end
+# Install the mod_php5 Apache module.
+httpd_module 'php5' do
+  instance 'customers'
+end
+
+# Install php5-mysql.
+package 'php5-mysql' do
+  action :install
+  notifies :restart, 'httpd_service[customers]'
+end
+# Load the secrets file and the encrypted data bag item that holds the database password.
+password_secret = Chef::EncryptedDataBagItem.load_secret(node['awesome_customers']['passwords']['secret_path'])
+user_password_data_bag_item = Chef::EncryptedDataBagItem.load('passwords', 'db_admin_password', password_secret)
+# Write a default home page.
+template "#{node['awesome_customers']['document_root']}/index.php" do
+  source 'index.php.erb'
+  mode '0644'
+  owner node['awesome_customers']['user']
+  group node['awesome_customers']['group']
+  variables({
+    :database_password => user_password_data_bag_item['password']
+  })
+end
---
+++ cookbooks/maven/recipes/default.rb
@@ -0,0 +1,33 @@
+#
+# Cookbook Name:: maven
+# Recipe:: default
+#
+# Copyright 2015, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+include_recipe 'apt::default'
+apt_repository 'mesosphere' do
+  uri 'http://repos.mesosphere.io/ubuntu'
+  components ['main']
+  key 'E56151BF'
+  keyserver 'keyserver.ubuntu.com'
+  distribution node['lsb']['codename']
+  action :add
+  deb_src false
+end
+package 'mesosphere' do
+  action :install
+end
+file "/etc/mesos/zk" do
+  content "zk://"+node['ipaddress']+":2181/mesos"
+end
+file "/etc/zookeeper/conf/myid" do
+  content "1"
+end
+file "/etc/zookeeper/conf/zoo.cfg" do
+  content "server.1="+node['ipaddress']+":2888:3888"
+end
+file "/etc/mesos-master/quorum" do
+  content "1"
+end
