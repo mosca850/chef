4372d38b6a57a3f195d313ba332389bae606fc4e
root <root@ip-172-31-5-88.eu-west-1.compute.internal>
2015-10-09 12:56:22 +0100

    adding metadata

---
+++ maven/metadata.rb
@@ -0,0 +1,7 @@
+name             'mesos'
+maintainer       'YOUR_COMPANY_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures maven-slave'
+version          '0.1.0'
+depends 'apt', '~> 2.7.0'
