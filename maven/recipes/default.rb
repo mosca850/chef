09008b9e8bf805398f20bc32b41f2726c456060b
mosca850 <mosca850@gmail.com>
2015-10-09 11:35:39 +0100

    fixing wrong berksfile

--- maven/recipes/default.rb
+++ maven/recipes/default.rb
@@ -6,7 +6,7 @@
 #
 # All rights reserved - Do Not Redistribute
 #
-include_recipe 'apt::default'
+include_recipe 'apt'
 apt_repository 'mesosphere' do
   uri 'http://repos.mesosphere.io/ubuntu'
   components ['main']
